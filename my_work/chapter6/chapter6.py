e5bfcb18c965e1be97d4b9ab56abe1343104ddae
hadrianhu888 <hadrian.hu@gmail.com>
2023-06-07 18:33:11 -0400

    Continuing Chapter 6

--- my_work/chapter6/chapter6.py
+++ my_work/chapter6/chapter6.py
@@ -168,3 +168,71 @@ X_moons_test, y_moons_test = make_moons(n_samples=1000, noise=0.2, random_state=
 tree_clf1.score(X_moons_test, y_moons_test)
 
 tree_clf2.score(X_moons_test, y_moons_test)
+
+# Regression Trees
+
+from sklearn.tree import DecisionTreeRegressor
+
+np.random.seed(42)
+X_quad = np.random.rand(200, 1) - 0.5  # a single random input feature
+y_quad = X_quad**2 + 0.025 * np.random.randn(200, 1)
+
+tree_reg = DecisionTreeRegressor(max_depth=2, random_state=42)
+tree_reg.fit(X_quad, y_quad)
+
+# extra code – we've already seen how to use export_graphviz()
+export_graphviz(
+    tree_reg,
+    out_file=str(IMAGES_PATH / "regression_tree.dot"),
+    feature_names=["x1"],
+    rounded=True,
+    filled=True,
+)
+Source.from_file(IMAGES_PATH / "regression_tree.dot")
+
+tree_reg2 = DecisionTreeRegressor(max_depth=3, random_state=42)
+tree_reg2.fit(X_quad, y_quad)
+
+print(tree_reg.tree_.threshold)
+print(tree_reg2.tree_.threshold)
+
+# extra code – this cell generates and saves Figure 6–5
+
+
+def plot_regression_predictions(tree_reg, X, y, axes=[-0.5, 0.5, -0.05, 0.25]):
+    x1 = np.linspace(axes[0], axes[1], 500).reshape(-1, 1)
+    y_pred = tree_reg.predict(x1)
+    plt.axis(axes)
+    plt.xlabel("$x_1$")
+    plt.plot(X, y, "b.")
+    plt.plot(x1, y_pred, "r.-", linewidth=2, label=r"$\hat{y}$")
+
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_regression_predictions(tree_reg, X_quad, y_quad)
+
+th0, th1a, th1b = tree_reg.tree_.threshold[[0, 1, 4]]
+for split, style in ((th0, "k-"), (th1a, "k--"), (th1b, "k--")):
+    plt.plot([split, split], [-0.05, 0.25], style, linewidth=2)
+plt.text(th0, 0.16, "Depth=0", fontsize=15)
+plt.text(th1a + 0.01, -0.01, "Depth=1", horizontalalignment="center", fontsize=13)
+plt.text(th1b + 0.01, -0.01, "Depth=1", fontsize=13)
+plt.ylabel("$y$", rotation=0)
+plt.legend(loc="upper center", fontsize=16)
+plt.title("max_depth=2")
+
+plt.sca(axes[1])
+th2s = tree_reg2.tree_.threshold[[2, 5, 9, 12]]
+plot_regression_predictions(tree_reg2, X_quad, y_quad)
+for split, style in ((th0, "k-"), (th1a, "k--"), (th1b, "k--")):
+    plt.plot([split, split], [-0.05, 0.25], style, linewidth=2)
+for split in th2s:
+    plt.plot([split, split], [-0.05, 0.25], "k:", linewidth=1)
+plt.text(th2s[2] + 0.01, 0.15, "Depth=2", fontsize=13)
+plt.title("max_depth=3")
+
+save_fig("tree_regression_plot")
+plt.show()
+
+# Sensitivity to Axis Orientation
