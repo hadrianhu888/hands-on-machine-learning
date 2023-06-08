cf2a33b701d867792aac2966f7fccd430041de30
hadrianhu888 <hadrian.hu@gmail.com>
2023-06-08 15:17:52 -0400

    Continuing Chapter 6

--- my_work/chapter6/chapter6.py
+++ my_work/chapter6/chapter6.py
@@ -236,3 +236,85 @@ save_fig("tree_regression_plot")
 plt.show()
 
 # Sensitivity to Axis Orientation
+
+np.random.seed(6)
+X_square = np.random.rand(100, 2) - 0.5
+y_square = (X_square[:, 0] > 0).astype(np.float32) * 2
+
+angle = np.pi / 4
+rotation_matrix = np.array(
+    [[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]]
+)
+
+X_rotated_square = X_square.dot(rotation_matrix)
+
+tree_clf_square = DecisionTreeClassifier(random_state=42)
+tree_clf_square.fit(X_square, y_square)
+tree_clf_rotated_square = DecisionTreeClassifier(random_state=42)
+tree_clf_rotated_square.fit(X_rotated_square, y_square)
+
+fig, axes = plt.subplots(ncols=2, figsize=(10, 4), sharey=True)
+plt.sca(axes[0])
+plot_decision_boundary(
+    tree_clf_square, X_square, y_square, axes=[-0.7, 0.7, -0.7, 0.7], cmap="Pastel1"
+)
+plt.sca(axes[1])
+plot_decision_boundary(
+    tree_clf_rotated_square,
+    X_rotated_square,
+    y_square,
+    axes=[-0.7, 0.7, -0.7, 0.7],
+    cmap="Pastel1",
+)
+plt.ylabel("")
+
+save_fig("sensitivity_to_rotation_plot")
+plt.show()
+
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+pca_pipeline = make_pipeline(StandardScaler(), PCA())
+X_iris_rotated = pca_pipeline.fit_transform(X_iris)
+tree_clf_pca = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf_pca.fit(X_iris_rotated, y_iris)
+
+# extra code – this cell generates and saves Figure 6–8
+
+plt.figure(figsize=(8, 4))
+
+axes = [-2.2, 2.4, -0.6, 0.7]
+z0s, z1s = np.meshgrid(
+    np.linspace(axes[0], axes[1], 100), np.linspace(axes[2], axes[3], 100)
+)
+X_iris_pca_all = np.c_[z0s.ravel(), z1s.ravel()]
+y_pred = tree_clf_pca.predict(X_iris_pca_all).reshape(z0s.shape)
+
+plt.contourf(z0s, z1s, y_pred, alpha=0.3, cmap=custom_cmap)
+for idx, (name, style) in enumerate(zip(iris.target_names, ("yo", "bs", "g^"))):
+    plt.plot(
+        X_iris_rotated[:, 0][y_iris == idx],
+        X_iris_rotated[:, 1][y_iris == idx],
+        style,
+        label=f"Iris {name}",
+    )
+
+plt.xlabel("$z_1$")
+plt.ylabel("$z_2$", rotation=0)
+th1, th2 = tree_clf_pca.tree_.threshold[[0, 2]]
+plt.plot([th1, th1], axes[2:], "k-", linewidth=2)
+plt.plot([th2, th2], axes[2:], "k--", linewidth=2)
+plt.text(
+    th1 - 0.01, axes[2] + 0.05, "Depth=0", horizontalalignment="right", fontsize=15
+)
+plt.text(
+    th2 - 0.01, axes[2] + 0.05, "Depth=1", horizontalalignment="right", fontsize=13
+)
+plt.axis(axes)
+plt.legend(loc=(0.32, 0.67))
+save_fig("pca_preprocessing_plot")
+
+plt.show()
+
+# Decision Trees Have High Variance
